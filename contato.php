fe78751858ec1a12f9943ee3cee876e89c64152f
mtonet <marcelotonet@gmail.com>
2017-10-18 13:20:30 -0200

    test push 2

--- contato.php
+++ contato.php
@@ -40,7 +40,7 @@
         <div class="col-md-6">
           <h2>Contato</h2>
           <hr>
-          <p>Selecionee o escritório que você deseja falar ou nos envie uma mensagem e esclareça todas as suas dúvidas sobre os nossos serviços e consulte nossos fretes.</p>
+          <p>Selecioneee o escritório que você deseja falar ou nos envie uma mensagem e esclareça todas as suas dúvidas sobre os nossos serviços e consulte nossos fretes.</p>
         </div>                        
       </div>
 <hr>
